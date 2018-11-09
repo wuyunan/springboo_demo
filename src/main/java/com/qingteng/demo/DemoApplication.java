251376f31728e2b3c5cd6247c36e78146dd7e35c
wuyunan <wuyunan@qingteng.info>
2018-11-09 17:58:56 +0800

    v1

--- src/main/java/com/qingteng/demo/DemoApplication.java
+++ src/main/java/com/qingteng/demo/DemoApplication.java
@@ -1,5 +1,6 @@
 package com.qingteng.demo;
 
+import com.qingteng.demo.model.Receiver;
 import com.qingteng.demo.storage.StorageProperties;
 import com.qingteng.demo.storage.StorageService;
 import org.springframework.boot.CommandLineRunner;
@@ -8,7 +9,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class DemoApplication {
@@ -25,5 +32,40 @@ public class DemoApplication {
             storageService.deleteAll();
             storageService.init();
         };
+    }
+
+
+    static final String topicExchangeName = "spring-boot-exchange";
+
+    static final String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
     }
 }
