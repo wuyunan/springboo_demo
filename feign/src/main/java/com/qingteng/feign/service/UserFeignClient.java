ed493a44550551817013df670e9b7a7060805cdf
wuyunan <wuyunan@qingteng.info>
2019-04-21 12:48:02 +0800

    v1

--- feign/src/main/java/com/qingteng/feign/service/UserFeignClient.java
+++ feign/src/main/java/com/qingteng/feign/service/UserFeignClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-@FeignClient(name = "microservice-provider-user")
+@FeignClient(name = "microservice-provider-user",url = "http://127.0.0.1:8000")
 public interface UserFeignClient {
 
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
--- microservice-consumer-movie-ribbon/src/main/java/com/qingteng/ribbon/service/RibbonService.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/qingteng/ribbon/service/RibbonService.java
@@ -1,6 +1,6 @@
-package com.qingteng.feign.service;
+package com.qingteng.ribbon.service;
 
-import com.qingteng.feign.bean.User;
+import com.qingteng.ribbon.bean.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
