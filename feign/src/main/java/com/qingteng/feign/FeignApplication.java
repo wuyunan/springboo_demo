8a03aebe1c77fc9a700f69fe19c04df922ec3f71
wuyunan <wuyunan@qingteng.info>
2019-04-21 14:23:17 +0800

    v1

--- feign/src/main/java/com/qingteng/feign/FeignApplication.java
+++ feign/src/main/java/com/qingteng/feign/FeignApplication.java
@@ -3,13 +3,10 @@ package com.qingteng.feign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @EnableFeignClients
-@EnableEurekaClient
-
 @SpringBootApplication
 public class FeignApplication {
 
--- microservice-consumer-movie-ribbon/src/main/java/com/qingteng/ribbon/controller/MovieController.java
+++ microservice-consumer-movie-ribbon/src/main/java/com/qingteng/ribbon/controller/MovieController.java
@@ -6,10 +6,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -17,8 +21,12 @@ public class MovieController {
 
     @Autowired
     private RibbonService ribbonService;
+
     @Autowired
     private LoadBalancerClient loadBalancerClient;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/user/{id}")
     public User findById(@PathVariable Long id) {
@@ -31,4 +39,15 @@ public class MovieController {
         // 打印当前选择的是哪个节点
         MovieController.LOGGER.info("{}:{}:{}", serviceInstance.getServiceId(), serviceInstance.getHost(), serviceInstance.getPort());
     }
+
+
+
+
+    @RequestMapping("/service-instances/{applicationName}")
+    public List<ServiceInstance> serviceInstancesByApplicationName(
+            @PathVariable String applicationName) {
+        return this.discoveryClient.getInstances(applicationName);
+    }
+
+
 }
