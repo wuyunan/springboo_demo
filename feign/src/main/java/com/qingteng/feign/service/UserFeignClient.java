60bd3a0916c905278193678d18e2a782297840a4
wuyunan <wuyunan@qingteng.info>
2019-04-21 22:44:42 +0800

    v1

--- feign/src/main/java/com/qingteng/feign/service/UserFeignClient.java
+++ feign/src/main/java/com/qingteng/feign/service/UserFeignClient.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @FeignClient(name = "microservice-provider-user")
 public interface UserFeignClient {
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "users/{id}", method = RequestMethod.GET)
     public User findById(@PathVariable("id") Long id);
 }
