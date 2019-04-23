48be4856625fd878ba50b71b7c5d08932c646c66
wuyunan <wuyunan@qingteng.info>
2019-04-23 14:23:43 +0800

    v1

---
+++ microservice-gateway-zuul/src/main/java/com/qingteng/microservice/gateway/MicroserviceGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package com.qingteng.microservice.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceGatewayZuulApplication.class, args);
+    }
+
+}
