906eadf07bab77182e281ea7003fd217732096b2
wuyunan <wuyunan@qingteng.info>
2019-04-24 15:19:50 +0800

    docker compose

--- microservice-config-server/src/main/java/com/qingteng/microservice/configserver/ConfigServerApplication.java
+++ microservice-config-server/src/main/java/com/qingteng/microservice/configserver/ConfigServerApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceConfigServerApplication {
+public class ConfigServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+        SpringApplication.run(ConfigServerApplication.class, args);
     }
 
 }
