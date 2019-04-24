6542d2cbceafe2482f582829d7756faffb043b5c
wuyunan <wuyunan@qingteng.info>
2019-04-24 12:10:28 +0800

    v1

---
+++ microservice-config-server/src/main/java/com/qingteng/microservice/configserver/MicroserviceConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.qingteng.microservice.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceConfigServerApplication.class, args);
+    }
+
+}
